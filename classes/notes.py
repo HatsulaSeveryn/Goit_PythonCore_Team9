f352229a1c6b6a627b69e951011bd9456769a168
Igor-Mozharov <r5@ukr.net>
2022-11-11 14:47:26 +0200

    add tags

--- classes/notes.py
+++ classes/notes.py
@@ -1,1 +1,8 @@
-from field import Field
+
+class Notes:
+    def __init__(self, note):
+        self.note = note
+        self.tags = []
+
+    def add_tags(self, tag):
+        self.tags.append(tag)
