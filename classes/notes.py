25ca2d8c899749675dafa56864af6ded2c21cd5f
Igor-Mozharov <r5@ukr.net>
2022-11-11 23:34:49 +0200

    find note_by_tag + word

--- classes/notes.py
+++ classes/notes.py
@@ -5,12 +5,20 @@ class NoteBook(UserDict):
     def add_note(self, note):
         self.data[note.title] = note
 
-    def find_note(self, word):
+    def find_note_by_word(self, word):
         result = []
         for title, note in self.data.items():
-            if word in title:
+            if word.lower() in title.lower():
                 result.append(note)
         return result
+
+    def find_note_by_tag(self, tag):
+        result = []
+        for note in self.data.values():
+            new_note_tags = [x.lower() for x in note.tags]
+            if tag.lower() in new_note_tags:
+                result.append(note)
+        return sorted(result, key=lambda x: x.title.lower())
 
 
 class Note:
@@ -28,8 +36,20 @@ class Note:
 
 notebook = NoteBook()
 
-first = Note('Life planet beautiful', 'Its about our life and other...')
-second = Note('Animal planet', 'Need to by some puppy')
+first = Note('life planet beautiful', 'Its about our life and other...')
+first.add_tags('Life')
+first.add_tags('Planet')
+second = Note('Animal', 'Need to by some puppy')
+second.add_tags('plaNET')
+fife = Note('Bear good', 'gooood')
+fife.add_tags('planet')
+six = Note('Ceylon tea', 'my favourite')
+six.add_tags('PLAnet')
 notebook.add_note(first)
 notebook.add_note(second)
-print(notebook.find_note('planet'))
+notebook.add_note(fife)
+notebook.add_note(six)
+nine = Note('Ellias', 'bad bad')
+nine.add_tags('PlaneT')
+notebook.add_note(nine)
+print(notebook.find_note_by_tag('Planet'))
