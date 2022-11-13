688b3dc599a4a0765f43269f6a1b33669cb15fbc
Igor-Mozharov <r5@ukr.net>
2022-11-13 16:36:23 +0200

    + show all

--- classes/note+NoteBook.py
+++ classes/note+NoteBook.py
@@ -16,13 +16,18 @@ class NoteBook(UserDict):
     def show_note(self, title):
         return self.data.get(title, 'this note doesnt exist')
 
-    def show_all_notes(self, page_number, page_size, flag=None):
+    def show_all_notes(self, flag=None):
         flag = True if flag == '-r' else False
-        page_number, page_size = int(page_number), int(page_size)
-        data_new = list(self.data.items())
-        all_note = page_number * page_size
-        # yield --> return
-        yield sorted(list(data_new[(all_note - page_size):all_note]), key=lambda x: x[0].lower(), reverse=flag)
+        count = 0
+        constant_number = 5
+        data_new = sorted(list(self.data.items()), reverse=flag)
+        while count < len(data_new):
+            print(data_new[count:count + constant_number])
+            user_input = input('nex 5 ?  ')
+            if user_input == 'exit':
+                break
+            else:
+                count += constant_number
 
     def find_note_by_title(self, word):
         return [note for title, note in self.data.items() if word.lower() in title.lower()]
@@ -33,30 +38,30 @@ class NoteBook(UserDict):
             x.lower() for x in note.tags]]
         return sorted(result, key=lambda x: x.title.lower(), reverse=flag)
 
-    @check_title
+    @ check_title
     def delete_note(self, title):
         self.data.pop(title)
 
-    @check_title
+    @ check_title
     def edit_text(self, title, new_text):
         self.data[title].text = new_text
 
-    @check_title
+    @ check_title
     def add_text(self, title, new_words):
         self.data[title].text += '. ' + new_words
 
-    @check_title
+    @ check_title
     def add_tag(self, title, new_tag):
         self.data[title].tags.append(new_tag)
 
-    @check_title
+    @ check_title
     def remove_tag(self, title, target_tag):
         result = ''.join(list(filter(lambda x: target_tag.lower()
                                      == x.lower(), self.data[title].tags)))
         if result:
             self.data[title].tags.remove(result)
 
-    @check_title
+    @ check_title
     def change_tag(self, title, old_tag, new_tag):
         result = ''.join(list(filter(lambda x: old_tag.lower()
                                      == x.lower(), self.data[title].tags)))
@@ -84,6 +89,12 @@ notebook.add_note('Bridge')
 notebook.add_note('ellisium')
 notebook.add_note('doctor')
 
+notebook.add_note('1')
+notebook.add_note('2')
+notebook.add_note('3')
+notebook.add_note('4')
+notebook.add_note('5')
+
 
 notebook.edit_text('Apple', 'Very good fruit')
 notebook.edit_text('Candy', 'my favourite')
@@ -100,4 +111,4 @@ notebook.add_tag('Bridge', 'place')
 notebook.add_tag('Apple', 'place')
 
 notebook.change_tag('doctor', 'peopLE', 'HUMANS')
-print(notebook)
+notebook.show_all_notes('-r')
